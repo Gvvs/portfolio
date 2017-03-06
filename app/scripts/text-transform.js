1b4ef4e32ee2a4b38588a0f5f1303bd0dac57659
Allan Dollé <dolle.allan@gmail.com>
2017-03-06 21:16:33 +0100

    Add some files and edit some. A usefull commit !

--- app/scripts/text-transform.js
+++ app/scripts/text-transform.js
@@ -23,16 +23,3 @@
 })(jQuery);
 // END Split 'transform-heading-letter' class into span for each caracters
 // ------
-
-// ------
-// BEGIN on document ready
-$(document).on('ready', function() {
-  console.log('document is ready');
-  setTimeout(function() {
-    $('.letter').addClass('loaded');
-    $('.anime-heading').addClass('loaded');
-    console.log('header is loaded and animation is started');
-  }, 1000);
-});
-// END on document ready
-// ------
