379e7669e64b99a41970a03e285664d8e447e6fe
Allan Dollé <dolle.allan@gmail.com>
2017-03-23 00:27:11 +0100

    V2 started, header done

--- app/scripts/text-transform.js
+++ app/scripts/text-transform.js
@@ -1,25 +1,25 @@
 // ------
 // BEGIN Split 'transform-heading-letter' class into span for each caracters
-(function($) {
-  var s,
-    spanizeLetters = {
-      settings: {
-        letters: $('.transform-heading-letter')
-      },
-      init: function() {
-        s = this.settings;
-        this.bindEvents();
-      },
-      bindEvents: function() {
-        s.letters.html(function(i, el) {
-          var spanizer = $.trim(el).split('');
-          return '<span class="letter">' +
-            spanizer.join('</span><span class="letter">') +
-            '</span>';
-        });
-      }
-    };
-  spanizeLetters.init();
-})(jQuery);
+// (function($) {
+//   var s,
+//     spanizeLetters = {
+//       settings: {
+//         letters: $('.transform-heading-letter')
+//       },
+//       init: function() {
+//         s = this.settings;
+//         this.bindEvents();
+//       },
+//       bindEvents: function() {
+//         s.letters.html(function(i, el) {
+//           var spanizer = $.trim(el).split('');
+//           return '<span class="letter">' +
+//             spanizer.join('</span><span class="letter">') +
+//             '</span>';
+//         });
+//       }
+//     };
+//   spanizeLetters.init();
+// })(jQuery);
 // END Split 'transform-heading-letter' class into span for each caracters
 // ------
