104dd50afea4d2827b97887bcb37a4ee01110d97
Allan Dollé <dolle.allan@gmail.com>
2017-03-30 22:21:15 +0200

    Add CV to index and sitemap

--- app/scripts/tracking.js
+++ app/scripts/tracking.js
@@ -29,6 +29,7 @@ class Track {
 
 // Instances of Track class
 var takeMeTo = new Track($("a[data-track='TakeMeTo']"));
+var cv = new Track($("a[data-track='Curriculum vitae']"));
 var social = new Track($("a[data-track='Social']"));
 var discover = new Track($("a[data-track='Discover']"));
 var credits = new Track($("a[data-track='Credit']"));
